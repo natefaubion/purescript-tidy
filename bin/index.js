ffb64f0bb1ea6fe734545c26a609baff9b32c6e8
Gary Burgess <gary.burgess@gmail.com>
2022-07-25 02:31:54 +0100

    Fix TIDY_INSTALL_LOC resolution on Windows

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import path from "path";
+import url from "url";
 import { main } from "../bundle/Main/index.js";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-process.env["TIDY_INSTALL_LOC"] = path.resolve(__dirname, "..");
+process.env["TIDY_INSTALL_LOC"] = url.fileURLToPath(new URL('..', import.meta.url));
 
 main();
