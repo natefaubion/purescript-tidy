83ec21015fe43f999882f547156e29dfcb0fd7d2
Serhii Khoma <srghma@gmail.com>
2024-03-18 14:25:24 +0700

    feat: GenerateDefaultOperatorsModule -> drop sunde lib, fix empty stdout using infinity function

--- script/src/GenerateDefaultOperatorsModule.purs
+++ script/src/GenerateDefaultOperatorsModule.purs
@@ -3,27 +3,25 @@ module GenerateDefaultOperatorsModule where
 import Prelude
 
 import Data.Argonaut.Core (Json)
-import Data.Argonaut.Decode (JsonDecodeError)
 import Data.Argonaut.Decode as Data.Argonaut.Decode
 import Data.Array (foldMap, mapWithIndex)
 import Data.Array as Array
 import Data.Either (Either(..), either)
 import Data.Maybe (Maybe(..))
+import Data.Number (infinity)
 import Data.String (Pattern(..))
 import Data.String as Str
 import Data.String as String
 import Data.String.Regex as Regex
 import Data.String.Regex.Flags (noFlags)
 import Data.String.Regex.Unsafe (unsafeRegex)
--- import Debug as Debug
 import Effect (Effect)
 import Effect.Aff (Aff, error, runAff_, throwError)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Console
-import Effect.Exception (catchException, throwException)
+import Effect.Exception (throwException)
 import Foreign.Object (Object)
 import Foreign.Object as Foreign.Object
-import Node.Buffer (Buffer)
 import Node.Buffer as Buffer
 import Node.ChildProcess as ChildProcess
 import Node.ChildProcess as Exec
@@ -35,8 +33,6 @@ import Node.Library.Execa as Node.Library.Execa
 import Node.Path (FilePath)
 import Node.Path as Path
 import Node.Process as Node.Process
-import Sunde as Sunde
-import Unsafe.Coerce (unsafeCoerce)
 
 foreign import tmpdir :: String -> Effect String
 
@@ -62,57 +58,23 @@ main = runAff_ (either throwException mempty) do
   let genCmdFile = Path.concat [ cwdPath, "bin", "index.js" ]
   let genCmdArgs = [ "generate-operators" ] <> pursFiles
 
-  liftEffect $ Console.log $ "Using command " <> Array.intercalate " " ([ genCmdFile ] <> genCmdArgs) <> "\n"
+  -- liftEffect $ Console.log $ "Using command " <> Str.joinWith " " ([ genCmdFile ] <> genCmdArgs) <> "\n"
 
-  -- | genCmdResult <- Node.Library.Execa.execa genCmdFile genCmdArgs (\x -> Debug.spy "options" (x { cwd = Just tmpPath, stripFinalNewline = Just true, maxBuffer = Nothing })) >>= \spawned -> spawned.getResult
-  -- genCmdResult <- liftEffect $ Node.Library.Execa.execaSync genCmdFile genCmdArgs (\x -> Debug.spy "options" (x { cwd = Just tmpPath, stripFinalNewline = Just true, maxBuffer = Just 99999999999.0 }))
-  -- | Console.log (unsafeCoerce genCmdResult)
-  -- | output <-
-  -- |   case genCmdResult.exit of
-  -- |     Node.ChildProcess.Types.Normally 0 -> pure genCmdResult.stdout
-  -- |     -- Node.ChildProcess.Types.Normally 0 -> pure genCmdResult.message
-  -- |     _ -> liftEffect do
-  -- |       Console.error genCmdResult.message
-  -- |       Node.Process.exit' 1
-
-  genCmdResult <- Sunde.spawn { cmd: genCmdFile, args: genCmdArgs, stdin: Nothing } identity
-  -- | Console.log (unsafeCoerce genCmdResult)
+  genCmdResult <- liftEffect $ Node.Library.Execa.execaSync genCmdFile genCmdArgs
+    ( _
+        { cwd = Just tmpPath
+        , maxBuffer = Just infinity -- The Nothing means 100MB, will result in empty stdout (but Nothing should mean Infinity https://github.com/JordanMartinez/purescript-node-execa/issues/20)
+        }
+    )
   output <-
     case genCmdResult.exit of
       Node.ChildProcess.Types.Normally 0 -> pure genCmdResult.stdout
       _ -> liftEffect do
-        Console.error genCmdResult.stdout
-        Console.error genCmdResult.stderr
+        Console.error genCmdResult.message
         Node.Process.exit' 1
 
-  -- file:///home/srghma/projects/purescript-tidy/output/Effect.Aff/foreign.js:530
-  -- throw util.fromLeft(step);
-  -- ^
-  -- TypeError: Cannot read properties of null (reading 'toString')
-  -- at Module.toStringImpl (file:///home/srghma/projects/purescript-tidy/output/Node.Buffer.Immutable/foreign.js:37:49)
-  -- at file:///home/srghma/projects/purescript-tidy/output/Node.Buffer.Immutable/index.js:25:25
-  -- at file:///home/srghma/projects/purescript-tidy/output/Control.Monad/index.js:70:33
-  -- at file:///home/srghma/projects/purescript-tidy/output/Effect/foreign.js:10:14
-  -- at file:///home/srghma/projects/purescript-tidy/output/Effect/foreign.js:10:20
-  -- at __do (file:///home/srghma/projects/purescript-tidy/output/GenerateDefaultOperatorsModule/index.js:104:124)
-  -- at file:///home/srghma/projects/purescript-tidy/output/Effect.Exception/foreign.js:38:22
-  -- at file:///home/srghma/projects/purescript-tidy/output/Effect/foreign.js:10:16
-  -- at runSync (file:///home/srghma/projects/purescript-tidy/output/Effect.Aff/foreign.js:88:20)
-  -- at run (file:///home/srghma/projects/purescript-tidy/output/Effect.Aff/foreign.js:326:22)
-  --
-  -- catchException
-  -- ( \err -> do
-  --     Console.log "Caught exception:"
-  --     stdout <- Buffer.toString UTF8 ((unsafeCoerce err).stdout :: Buffer)
-  --     stderr <- Buffer.toString UTF8 ((unsafeCoerce err).stderr :: Buffer)
-  --     Console.log stdout
-  --     Console.error stderr
-  --     throwException err
-  -- )
-  -- (ChildProcess.execSync' genCmd optsModifier)
-
-  liftEffect $ Console.log "output:"
-  liftEffect $ Console.log output
+  -- liftEffect $ Console.log "output:"
+  -- liftEffect $ Console.log output
 
   let
     header =
